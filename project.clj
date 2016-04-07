d0950555eb16b99261c622493c45553aa023ffce
Camilo Roca <carocad@unal.edu.co>
2016-04-07 20:57:25 +0200

    frechet-distance dependency updated to latest version incanter and clojure.data dependencies uncommented for development

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "LGPLv3"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [frechet-dist "0.7.2"]
+                 [frechet-dist "0.8.0"]
                  ; to create geojson objects
                  [org.clojure/data.json "0.2.6"]
                  ; to send http requests to mapbox
@@ -18,9 +18,9 @@
                          ; read test data
                          [org.clojure/data.csv "0.1.3"]
                          ; create charts
-                         ;[incanter/incanter-charts "1.5.5"]
+                         [incanter/incanter-charts "1.5.5"]
                          ; encode charts to base64
-                         ;[org.clojure/data.codec "0.1.0"]
+                         [org.clojure/data.codec "0.1.0"]
                          ; profiler
                          ;[com.taoensso/timbre "4.3.1"]
                           ]}})
