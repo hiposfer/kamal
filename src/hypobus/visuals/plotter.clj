d6c3d076da929bf12319fb3467b4dd77a0ef3d83
Camilo Roca <carocad@unal.edu.co>
2016-04-07 20:58:56 +0200

    uncommented plotting functions for development

--- src/hypobus/visuals/plotter.clj
+++ src/hypobus/visuals/plotter.clj
@@ -1,22 +1,24 @@
-(ns hypobus.visuals.plotter)
-;;   (:import  [org.jfree.chart ChartUtilities JFreeChart])
-;;   (:require [clojure.data.codec.base64 :as b64]
-;;             [incanter.charts :as chart]))
+(ns hypobus.visuals.plotter
+  (:import  [org.jfree.chart ChartUtilities JFreeChart])
+  (:require [clojure.data.codec.base64 :as b64]
+            [incanter.charts :as chart]))
 
-;; (defn- plot
-;;   [^JFreeChart chart & options]
-;;   (let [opts      (when options (apply assoc {} options))
-;;         width     (or (:width opts) 400)
-;;         height    (or (:height opts) 300)
-;;         ; byte array with binary PNG data
-;;         image-buf (ChartUtilities/encodeAsPNG (.createBufferedImage chart width height))]
-;;      (clojure.string/join (map char (b64/encode image-buf)))))
+(defn- plot
+  [^JFreeChart chart & options]
+  (let [opts      (when options (apply assoc {} options))
+        width     (or (:width opts) 400)
+        height    (or (:height opts) 300)
+        ; byte array with binary PNG data
+        image-buf (ChartUtilities/encodeAsPNG (.createBufferedImage chart width height))]
+     (clojure.string/join (map char (b64/encode image-buf)))))
 
-;; (defn show-polyline
-;;   ([coll]
-;;   (let [image (chart/scatter-plot (map :lon coll) (map :lat coll))]
-;;     (plot image)))
-;;   ([coll groups]
-;;   (let [image (chart/scatter-plot (map :lon coll) (map :lat coll)
-;;                                   :group-by groups)]
-;;     (plot image))))
+(defn show-polyline
+  ([coll]
+  (let [image (chart/scatter-plot (map :lon coll) (map :lat coll))]
+    (plot image)))
+  ([coll groups]
+  (let [image (chart/scatter-plot (map :lon coll) (map :lat coll)
+                                  :group-by groups)]
+    (plot image))))
+
+;; (apply str (map char (b64/encode (.getBytes (slurp "https://raw.githubusercontent.com/carocad/nerdy-painter/master/resources/sprout-chart.svg")))))
