a08c1f6fc91e0ceb2c4dc965708cf758d45fe9e5
carocad <carocad@unal.edu.co>
2017-06-16 17:10:49 +0200

    added test.check readme example to check bug

--- test/backend/prop_test.clj
+++ test/backend/prop_test.clj
@@ -5,20 +5,23 @@
             [clojure.test.check.clojure-test :refer [defspec]]
             [backend.routing.algorithms :as alg]
             [backend.routing.core :as core]
-            [backend.generators :as g]
-            [backend.specs :as core.specs])) ;; loads the spec in the registry
+            [backend.generators :as g]))
 
 ; -------------------------------------------------------------------
 ; The Dijkstra algorithm is deterministic, therefore for the same src/dst
 ; combination it should return the same path
 ; path(src,dst) = path(src, dst)
-(def deterministic
-  ;300; tries
-  (prop/for-all [graph (g/graph 10)
-                 src   (gen/elements (keys graph))
-                 dst   (gen/elements (keys graph))]
-    (let [router (core/->ArcLengthRouter src dst :out-arcs)]
-      (apply = (repeatedly (alg/dijkstra-1d graph router (core/sources router) :out-arcs)
-                           10)))))
+;(def deterministic
+;  ;300; tries
+;  (prop/for-all [graph (g/graph 10)]
+;    (let [router (core/->ArcLengthRouter src dst :out-arcs)
+;          (apply = (repeatedly (alg/dijkstra-1d graph router (core/sources router) :out-arcs)
+;                           10))])))
 
 ;(tc/quick-check 100 deterministic)
+
+(def sort-idempotent-prop
+  (prop/for-all [v (gen/vector gen/int)]
+                (= (sort v) (sort (sort v)))))
+
+(tc/quick-check 100 sort-idempotent-prop)
