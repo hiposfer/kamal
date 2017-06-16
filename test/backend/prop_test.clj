6a0d03b0f35baa2182dd6fc535e76a3a9a62f816
carocad <carocad@unal.edu.co>
2017-06-16 17:31:12 +0200

    removed test.check example spec for deterministic dijkstra behavior

--- test/backend/prop_test.clj
+++ test/backend/prop_test.clj
@@ -11,17 +11,13 @@
 ; The Dijkstra algorithm is deterministic, therefore for the same src/dst
 ; combination it should return the same path
 ; path(src,dst) = path(src, dst)
-;(def deterministic
-;  ;300; tries
-;  (prop/for-all [graph (g/graph 10)]
-;    (let [router (core/->ArcLengthRouter src dst :out-arcs)
-;          (apply = (repeatedly (alg/dijkstra-1d graph router (core/sources router) :out-arcs)
-;                           10))])))
+(defspec deterministic
+  100; tries
+  (prop/for-all [graph (g/graph 10)]
+    (let [src (rand-nth (keys graph))
+          dst (rand-nth (keys graph))
+          router (core/->ArcLengthRouter src dst :out-arcs)
+          (apply = (repeatedly (alg/dijkstra-1d graph router (core/sources router) :out-arcs)
+                           10))])))
 
 ;(tc/quick-check 100 deterministic)
-
-(def sort-idempotent-prop
-  (prop/for-all [v (gen/vector gen/int)]
-    (= (sort v) (sort (sort v)))))
-
-;(tc/quick-check 100 sort-idempotent-prop)
