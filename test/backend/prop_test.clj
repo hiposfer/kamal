6ec6e460c29a8f054874129900d4abc9850a95d2
carocad <carocad@unal.edu.co>
2017-06-16 17:18:32 +0200

    exclude spec ns from eastwood check

--- test/backend/prop_test.clj
+++ test/backend/prop_test.clj
@@ -22,6 +22,6 @@
 
 (def sort-idempotent-prop
   (prop/for-all [v (gen/vector gen/int)]
-                (= (sort v) (sort (sort v)))))
+    (= (sort v) (sort (sort v)))))
 
 ;(tc/quick-check 100 sort-idempotent-prop)
