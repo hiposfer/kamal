eb6a7a1c5a37b3cba71750d3cb8223c692779e61
carocad <carocad@unal.edu.co>
2017-06-16 18:42:35 +0200

    removed bidirectional dijkstra and old comments

--- src/backend/routing/algorithms.clj
+++ src/backend/routing/algorithms.clj
@@ -50,39 +50,6 @@
               resettled     (persistent! (assoc! (transient settled) curr-id trz))]
           (recur resettled (route/stop? router resettled curr-id)))))))
 
-;TODO: promise less do more. This algorithm is not yet reliable
-;(defn dijkstra-2d
-;  "computes the shortest path between a set of sources and any other node in graph
-;  using a Bidirectional (2d) Dijkstra's algorithm with a priority queue. Stops
-;  whenever the router fulfills its matching criteria or when no more nodes can
-;  be reached. Returns a vector of
-;  [success? settled-forward unsettled-forward settled-backward unsettled-backward]"
-;  [graph router]
-;  (let [vanguard  (init-queue (route/sources router))
-;        rearguard (init-queue (route/destinations router))]
-;    (loop [forward  (imap/int-map)
-;           backward (imap/int-map)
-;           halt     false]
-;      (fast-forward! vanguard forward) ;; ignore settled nodes
-;      (fast-forward! rearguard backward) ;; ignore settled nodes
-;      (if-not (or halt (.isEmpty vanguard) (.isEmpty rearguard))
-;        (let [[fid f-trz] (.poll vanguard) ;; forward
-;              [bid b-trz] (.poll rearguard) ;; backward
-;              _           (step! graph :out-arcs router forward vanguard fid f-trz)
-;              _           (step! graph :in-arcs router backward rearguard bid b-trz)
-;              refor       (persistent! (assoc! (transient forward) fid f-trz))
-;              reback      (persistent! (assoc! (transient backward) bid b-trz))]
-;          (recur refor reback (route/stop? router forward fid backward bid)))
-;        [halt
-;         forward
-;         (into (imap/int-map)
-;               (comp (utils/unique-by first) (remove #(contains? forward (first %))))
-;               vanguard)
-;         backward
-;         (into (imap/int-map)
-;               (comp (utils/unique-by first) (remove #(contains? backward (first %))))
-;               rearguard)]))))
-
 (defn dijkstra
   [graph router]
   (condp = (route/direction router)
@@ -108,19 +75,7 @@
   (reduce-kv (fn [best _ node] (min-key #(utils/euclidean-pow2 point %) node best))
              (second (first graph)) graph))
 
-(defn pbrute-nearest
-  "search the nearest node in graph to point using the euclidean function
-  WARNING: This function does NOT perform well on small graphs probably due to
-  the euclidean pow2 not being a computationally intensive function"
-  [graph point]
-  (println (:lat point) (:lon point))
-  (let [geo-dist #(utils/euclidean-pow2 point %)
-        reducef   (fn [best _ node] (min-key geo-dist node best))
-        combinef (fn ([] (second (first graph)))
-                     ([v] (apply min-key geo-dist v)))]
-    (clojure.core.reducers/fold combinef reducef graph)))
-
-(def foo (delay (time (osm/cleanup (osm/osm->graph "resources/osm/saarland.osm")))))
+;(def foo (delay (time (osm/cleanup (osm/osm->graph "resources/osm/saarland.osm")))))
 
 ;(let [point (rand-nth (vals @osm/foo))]
 ;  (time (brute-nearest @osm/foo point))
@@ -169,11 +124,6 @@
                             5 {:src 5 :length 9 :kind :other}}}})
 
 ;(time (path rosetta (dijkstra rosetta 1 5) 5))
-;(dijkstra rosetta (route/->ArcLengthRouter 1 5 ::route/bidirectional))
+;(dijkstra rosetta (route/->ArcLengthRouter 1 5 ::route/forward))
 ;Distances from 1: ((1 0) (2 7) (3 9) (4 20) (5 26) (6 11))
 ;Shortest path: (1 3 4 5)
-
-; (backend.utils.mapbox/write-geojson "resources/route.json"
-;   (sequence (comp (map #(get @osm/foo %))
-;                   (map #(select-keys % [:lon :lat])))
-;             foo))
