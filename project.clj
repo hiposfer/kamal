edb04305328c5c42e8d142df9606caa473d6f04e
carocad <carocad@unal.edu.co>
2017-06-25 01:51:51 +0200

    added direct linking comment flag for possible future optimization

--- project.clj
+++ project.clj
@@ -13,6 +13,8 @@
   :monkeypatch-clojure-test false
   :plugins [[jonase/eastwood "0.2.3"]]
   :jvm-opts ["-Xmx4g"]
+             ;; "-Dclojure.compiler.direct-linking=true
+             ;; https://github.com/clojure/clojure/blob/master/changes.md#11-direct-linking
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"] ;; generate test data
                                   [criterium "0.4.4"]]}} ;; benchmark
   :test-selectors {:default (complement :benchmark)
