620e696bdd3c805c0d6d9f20cffbf604519149e5
carocad <carocad@unal.edu.co>
2017-07-07 12:01:59 +0200

    moved math related utility functions to a separate namespace moved tool package inside routing for better encapsulation

--- src/service/routing/utils/tool.clj
+++ src/service/routing/utils/tool.clj
@@ -40,23 +40,3 @@
     (update-vals coll (range (count coll)) f)
     (update-vals coll (keys coll) f)))
   ([coll keys f] (reduce (fn [m k] (update m k f)) coll keys)))
-
-(def RADIOUS 6372800); radious of the Earth in meters
-
-(defn haversine
-  "Compute the great-circle distance between two points on Earth given their
-  longitude and latitude in RADIANS. The distance is computed in meters
-  by default."
-  [lon-1 lat-1 lon-2 lat-2]
-  (let [h  (+ (Math/pow (Math/sin (/ (- lat-2 lat-1) 2)) 2)
-              (* (Math/pow (Math/sin (/ (- lon-2 lon-1) 2)) 2)
-                 (Math/cos lat-2)
-                 (Math/cos lat-1)))]
-    (* RADIOUS 2 (Math/asin (Math/sqrt h)))))
-
-;; (frepos/distance [-86.67 36.12] [-118.40 33.94])
-;=> 2887.2599506071106 km
-; distance between paris and san francisco
-; (* (frepos/distance [2.33 48.87] [-122.4 37.8]) (/ 3440.069 6372))
-; => 4831.502535634215 nauticals miles
-
