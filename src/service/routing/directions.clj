8715fba485eb898ff5912d98c9522f0c2605a7ad
carocad <carocad@unal.edu.co>
2017-07-09 16:17:11 +0200

    TODOs added

--- src/service/routing/directions.clj
+++ src/service/routing/directions.clj
@@ -5,7 +5,7 @@
             [service.routing.graph.protocols :as rp]
             [service.routing.utils.math :as math]))
 
-(defn- length
+(defn length
   "A very simple value computation function for Arcs in a graph.
   Returns a SimpleValue with the length of the arc"
   [arc _]
@@ -53,6 +53,11 @@
 ;; but looking into the future it is good to make like this such that we
 ;; can always extend it with more parameters
 ;; https://www.mapbox.com/api-documentation/#retrieve-directions
+;; TODO: we still need to return the legs of a route
+;; see https://github.com/mapbox/mapbox-gl-directions/issues/133
+;; TODO: we need to handle special cases like
+;; - src/dst not found
+;; - shortest path not found (nil)
 (defn direction
   "given a graph and a sequence of keywordized parameters according to
    https://www.mapbox.com/api-documentation/#retrieve-directions
