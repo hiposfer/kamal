db906c6987300c5d523d50c3fcc1b9ff9e8ba07b
Mehdi Sadeghi <mehdi@mehdix.org>
2017-07-16 01:17:16 +0200

    Add api specs draft

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/data.xml "0.0.8"] ; parse xml lazily
                  [org.clojure/data.int-map "0.2.4"] ; fast integers-map
-                 [ring/ring-core "1.6.1"]
-                 [ring/ring-jetty-adapter "1.5.0"]]
+                 [metosin/compojure-api "2.0.0-alpha5"]
+                 [metosin/spec-tools "0.3.0"]]
   ;; Sets the values of global vars within Clojure.
   :global-vars {*warn-on-reflection* true
                 *print-length* 50}
@@ -15,7 +15,7 @@
   :monkeypatch-clojure-test false
   :plugins [[jonase/eastwood "0.2.3"]
             [lein-ring "0.12.0"]]
-  :ring {:handler service.routing.core/handler}
+  :ring {:handler service.routing.core/app}
   :jvm-opts ["-Xmx1g"]
              ;; "-Dclojure.compiler.direct-linking=true"
              ;; https://github.com/clojure/clojure/blob/master/changes.md#11-direct-linking
