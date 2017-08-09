074677e2a4b03793fb465034daadc091b336697b
Mehdi Sadeghi <mehdi@mehdix.org>
2017-08-10 01:51:24 +0200

    Move routes def out of specs
    Former-commit-id: cce81a3890b3d5db00101d51be3a19839f34d52f

--- src/service/routing/core.clj
+++ src/service/routing/core.clj
@@ -1,6 +1,41 @@
 (ns service.routing.core
-  (:require [compojure.api.sweet :refer :all]
-            [service.routing.spec]))
+  (:require [clojure.spec.gen.alpha :as gen]
+            [ring.util.http-response :refer [ok]]
+            [compojure.api.sweet :refer [context GET api]]
+            [service.routing.spec :as s]
+            [service.routing.directions :as dir]
+            [service.routing.graph.generators :as g]))
+            ;[expound.alpha :as expound]
+
+;(s/valid? (s/and ::radiuses-regex #(= (count coordinates) (count %)))))
+;(s/and ::s/radiuses-regex #(= (count coordinates) (count %))))
+
+(def routes
+  (context "/spec" []
+    :tags ["spec"]
+    :coercion :spec
+
+    (GET "/direction/:coordinates" []
+      :summary "direction with clojure.spec"
+      :path-params [coordinates :- ::s/coordinate-regex]
+      :query-params [{steps :- boolean? false}
+                     {radiuses :- ::s/radiuses-regex nil}
+                     {alternatives :- boolean? false}
+                     {language :- string? "en"}]
+      :return ::s/direction
+      (ok (let [coords (map zipmap (repeat [:lon :lat])
+                                   (s/parse-coordinates coordinates))
+                rads (when-not (nil? radiuses) (s/parse-radiuses radiuses))]
+            (dir/direction (gen/generate (g/graph 1000))
+              :coordinates coords
+              :steps steps
+              :radiuses rads
+              :alternatives alternatives
+              :language language))))))
+
+;;;; TEST @mehdi
+;(expound/expound-str ::direction (dir/direction (gen/generate (g/graph 1000)) :coordinates [{:lon 1 :lat 2} {:lon 3 :lat 4}]))
+;(s/explain ::direction (dir/direction (gen/generate (g/graph 1000)) :coordinates [{:lon 1 :lat 2} {:lon 3 :lat 4}]))
 
 (def app
   (api
@@ -11,6 +46,4 @@
                     :description "Routing for hippos"}
              :tags [{:name "direction", :description "direction similar to mabbox"}]}}}
 
-    service.routing.spec/routes))
-
-;;(println (direction (gen/generate (g/graph 1000)) :coordinates [{:lon 1 :lat 2} {:lon 3 :lat 4}]))
+    routes))
