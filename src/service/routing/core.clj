1737b2caf02235c6d14ce28f06ce31f06c0d975d
Camilo Roca <carocad@unal.edu.co>
2017-08-12 18:37:39 +0200

    cosmetic changes

--- src/service/routing/core.clj
+++ src/service/routing/core.clj
@@ -2,7 +2,7 @@
   (:require [clojure.spec.alpha :as s]
             [clojure.spec.gen.alpha :as gen]
             [ring.util.http-response :refer [ok]]
-            [compojure.api.sweet :refer [context GET api]]
+            [compojure.api.sweet :refer [context GET defapi]]
             [service.routing.spec :as spec]
             [service.routing.directions :as dir]
             [service.routing.graph.generators :as g]))
@@ -15,31 +15,29 @@
 ;(expound/expound-str ::direction (dir/direction (gen/generate (g/graph 1000)) :coordinates [{:lon 1 :lat 2} {:lon 3 :lat 4}]))
 ;(s/explain ::direction (dir/direction (gen/generate (g/graph 1000)) :coordinates [{:lon 1 :lat 2} {:lon 3 :lat 4}]))
 
-(def app
-  (api {:swagger {:ui "/"
-                  :spec "/swagger.json"
-                  :data {:info {:title "Routing API"
-                                :description "Routing for hippos"}
-                         :tags [{:name "direction", :description "direction similar to mabbox"}]}}}
-    (context "/spec" []
-      :tags ["spec"]
-      :coercion :spec
-      (GET "/direction/:coordinates" []
-        :summary "direction with clojure.spec"
-        :path-params [coordinates :- ::spec/coordinate-raw]
-        :query-params [{steps :- boolean? false}
-                       {radiuses :- ::spec/radiuses-raw nil}
-                       {alternatives :- boolean? false}
-                       {language :- string? "en"}]
-        :return ::spec/direction
-        (ok (if-not (s/valid? (s/and #(= (count coordinates) (count %))) radiuses)
-              {:message "The same amount of radiouses and coordinates must be provided"
-               :code "InvalidInput"}
-              (let [coords (map zipmap (repeat [:lon :lat]) coordinates)]
-                (dir/direction (gen/generate (g/graph 1000))
-                  :coordinates coords
-                  :steps steps
-                  :radiuses radiuses
-                  :alternatives alternatives
-                  :language language))))))))
+(defapi app
+  {:swagger {:ui "/"
+             :spec "/swagger.json"
+             :data {:info {:title "Routing API"
+                           :description "Routing for hippos"}
+                    :tags [{:name "direction", :description "direction similar to mabbox"}]}}}
+  (GET "spec/direction/:coordinates" []
+    :coercion :spec
+    :summary "direction with clojure.spec"
+    :path-params [coordinates :- ::spec/coordinate-raw]
+    :query-params [{steps :- boolean? false}
+                   {radiuses :- ::spec/radiuses-raw nil}
+                   {alternatives :- boolean? false}
+                   {language :- string? "en"}]
+    :return ::spec/direction
+    (ok (if-not (s/valid? (s/and #(= (count coordinates) (count %))) radiuses)
+          {:message "The same amount of radiouses and coordinates must be provided"
+           :code "InvalidInput"}
+          (let [coords (map zipmap (repeat [:lon :lat]) coordinates)]
+            (dir/direction (gen/generate (g/graph 1000))
+              :coordinates coords
+              :steps steps
+              :radiuses radiuses
+              :alternatives alternatives
+              :language language))))))
 
