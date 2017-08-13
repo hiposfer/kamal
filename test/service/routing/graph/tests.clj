bf1564db2471623cb3a41a3481384e254c6bc32a
Camilo Roca <carocad@unal.edu.co>
2017-08-13 23:30:23 +0200

    graph tests (#36)
    * unit tests added
    
    * generative tests added
    
    * rename length to duration

---
+++ test/service/routing/graph/tests.clj
@@ -0,0 +1,115 @@
+(ns service.routing.graph.tests
+  (:require [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :refer [defspec]]
+    ;[clojure.test.check :as tc]
+            [clojure.test.check.generators :as gen]
+            [clojure.test :refer [is deftest]]
+            [service.routing.graph.algorithms :as alg]
+            [service.routing.graph.protocols :as rp]
+            [service.routing.graph.generators :as g]
+            [service.routing.directions :as direction]
+            [service.routing.graph.core :as route]))
+
+;; https://rosettacode.org/wiki/Dijkstra%27s_algorithm
+(def rosetta {1 {:out-arcs [{:dst 2 :length 7  :kind :other}
+                            {:dst 3 :length 9  :kind :other}
+                            {:dst 6 :length 14 :kind :other}]
+                 :in-arcs  nil}
+              2 {:out-arcs [{:dst 3 :length 10 :kind :other}
+                            {:dst 4 :length 15 :kind :other}]
+                 :in-arcs  [{:src 1 :length 7  :kind :other}]}
+              3 {:out-arcs [{:dst 4 :length 11 :kind :other}
+
+                            {:dst 6 :length 2  :kind :other}]
+                 :in-arcs  [{:src 1 :length 9  :kind :other}
+                            {:src 2 :length 10 :kind :other}]}
+              4 {:out-arcs [{:dst 5 :length 6  :kind :other}]
+                 :in-arcs  [{:src 2 :length 15 :kind :other}
+                            {:src 3 :length 11 :kind :other}]}
+              5 {:out-arcs [{:dst 6 :length 9  :kind :other}]
+                 :in-arcs  [{:src 4 :length 6  :kind :other}]}
+              6 {:out-arcs nil
+                 :in-arcs  [{:src 1 :length 14 :kind :other}
+                            {:src 3 :length 2  :kind :other}
+                            {:src 5 :length 9  :kind :other}]}})
+
+;Distances from 1: ((1 0) (2 7) (3 9) (4 20) (5 26) (6 11))
+;Shortest path: (1 3 4 5)
+
+(deftest shortest-path
+  (let [dst       5
+        performer (alg/dijkstra rosetta
+                    :value-by (fn length [arc _] (route/->SimpleValue (:length arc)))
+                    :start-from #{1})
+        traversal (reduce (fn [_ v] (when (= dst (key v)) (reduced v)))
+                      nil
+                      performer)]
+    (is (not (nil? traversal)) "shortest path not found")
+    (is (= '(5 4 3 1) (map key (rp/path traversal))) "shortest path doesnt traverse expected nodes")))
+
+(deftest all-paths
+  (let [performer (alg/dijkstra rosetta
+                                :value-by (fn length [arc _] (route/->SimpleValue (:length arc)))
+                                :start-from #{1})
+        traversal (into {} (map (juxt key (comp rp/cost val)))
+                          performer)]
+    (is (not (nil? traversal)) "shortest path not found")
+    (is (= {1 0.0, 2 7.0, 3 9.0, 4 20.0, 5 26.0, 6 11.0} traversal) "shortest path doesnt traverse expected nodes")))
+
+; -------------------------------------------------------------------
+; The Dijkstra algorithm is deterministic, therefore for the same src/dst
+; combination it should return the same path
+; path(src,dst) = path(src, dst)
+(defspec deterministic
+  100; tries
+  (prop/for-all [graph (gen/such-that not-empty (g/graph 10) 1000)]
+    (let [src  (rand-nth (keys graph))
+          dst  (rand-nth (keys graph))
+          coll (alg/dijkstra graph :start-from #{src}
+                                   :value-by direction/duration)
+          results (for [i (range 10)]
+                    (reduce (fn [_ v] (when (= dst (key v)) (reduced v)))
+                            nil coll))]
+      (or (every? nil? results)
+          (and (apply = (map key results))
+               (apply = (map (comp rp/cost val) results)))))))
+
+; -------------------------------------------------------------------
+; The Dijkstra algorithm cost is monotonic (increasing)
+; https://en.wikipedia.org/wiki/Monotonic_function
+(defspec monotonic
+  100; tries
+  (prop/for-all [graph (gen/such-that not-empty (g/graph 10) 1000)]
+    (let [src  (rand-nth (keys graph))
+          dst  (rand-nth (keys graph))
+          coll (alg/dijkstra graph
+                             :start-from #{src}
+                             :value-by direction/duration)
+          result (reduce (fn [_ v] (when (= dst (key v)) (reduced v)))
+                         nil
+                         coll)]
+      (or (nil? result)
+          (apply >= (concat (map (comp rp/cost val)
+                                 (rp/path result))
+                            [0]))))))
+
+; -------------------------------------------------------------------
+; If the distance of two nodes is 0 and no edge has a 0 cost,
+; then the two nodes MUST be the same
+; Ddf(P,Q) = 0 if P = Q
+(defspec symmetry
+  100; tries
+  (prop/for-all [graph (gen/such-that not-empty (g/graph 10) 1000)]
+    (let [src  (rand-nth (keys graph))
+          coll (alg/dijkstra graph
+                             :start-from #{src}
+                             :value-by direction/duration)
+          result (reduce (fn [_ v] (when (= src (key v)) (reduced v)))
+                         nil
+                         coll)]
+      (and (not (nil? result))
+           (= 1 (count (rp/path result)))))))
+
+;(clojure.test/run-tests)
+
+;(tc/quick-check 100 deterministic)
