79a9471dc1bd6e93f56e9c07199f8520d54dff4d
carocad <carocad@unal.edu.co>
2017-08-31 22:20:21 +0200

    fix: haversine should accept values in degrees fix: mod is gausian mod not remainder as in ANSI C

--- src/service/routing/utils/math.clj
+++ src/service/routing/utils/math.clj
@@ -1,19 +1,28 @@
 (ns service.routing.utils.math
   (:require [service.routing.graph.protocols :as rp]))
 
+;; Note in these scripts, I generally use
+;; - latitude, longitude in degrees
+;; - φ for latitude  in radians
+;; - λ for longitude in radians
+;; having found that mixing degrees & radians is often the easiest route to
+;; head-scratching bugs...
 
 (def RADIOUS 6372800); radious of the Earth in meters
 
 (defn haversine
   "Compute the great-circle distance between two points on Earth given their
-  longitude and latitude in RADIANS. The distance is computed in meters
-  by default."
+  longitude and latitude in DEGREES. The distance is computed in meters"
   ([lon-1 lat-1 lon-2 lat-2]
-   (let [h  (+ (Math/pow (Math/sin (/ (- lat-2 lat-1) 2)) 2)
-               (* (Math/pow (Math/sin (/ (- lon-2 lon-1) 2)) 2)
-                  (Math/cos lat-2)
-                  (Math/cos lat-1)))]
-     (* RADIOUS 2 (Math/asin (Math/sqrt h)))))
+   (let [φ1 (Math/toRadians lat-1)
+         φ2 (Math/toRadians lat-2)
+         Δφ (Math/toRadians (- lat-2 lat-1))
+         Δλ (Math/toRadians (- lon-2 lon-1))
+         a  (+ (Math/pow (Math/sin (/ Δφ 2)) 2)
+               (* (Math/pow (Math/sin (/ Δλ 2)) 2)
+                  (Math/cos φ2)
+                  (Math/cos φ1)))]
+     (* RADIOUS 2 (Math/asin (Math/sqrt a)))))
   ([p1 p2]
    (cond
      (and (satisfies? rp/GeoCoordinate p1) (satisfies? rp/GeoCoordinate p2))
@@ -26,6 +35,12 @@
      (and (map? p1) (map? p2))
      (haversine (:lon p1) (:lat p1)
                 (:lon p2) (:lat p2)))))
+
+;; (frepos/distance [-86.67 36.12] [-118.40 33.94])
+;=> 2887.2599506071106 km
+; distance between paris and san francisco
+; (* (frepos/distance [2.33 48.87] [-122.4 37.8]) (/ 3440.069 6372))
+; => 4831.502535634215 nauticals miles
 
 (defn euclidean-pow2
   "computes the squared euclidean distance between p1 and p2 being both of them
@@ -41,11 +56,6 @@
   [p1 p2]
   (Math/sqrt (euclidean-pow2 p1 p2)))
 
-;; (frepos/distance [-86.67 36.12] [-118.40 33.94])
-;=> 2887.2599506071106 km
-; distance between paris and san francisco
-; (* (frepos/distance [2.33 48.87] [-122.4 37.8]) (/ 3440.069 6372))
-; => 4831.502535634215 nauticals miles
 
 (defn arc-length
   "given a sequence of [lon lat] points like those of geojson LineString
@@ -60,12 +70,12 @@
   "return a Number between 0 and 360 indicating the clockwise angle from true
    north to the direction of travel right before the maneuver"
   [p1 p2]
-  (let [lat1 (Math/toRadians (rp/lat p1))
-        lat2 (Math/toRadians (rp/lat p2))
-        delta-lon (Math/toRadians (- (rp/lon p2) (rp/lon p1)))
+  (let [φ1 (Math/toRadians (rp/lat p1))
+        φ2 (Math/toRadians (rp/lat p2))
+        Δλ (Math/toRadians (- (rp/lon p2) (rp/lon p1)))
         ; actual computation
-        y (* (Math/sin delta-lon) (Math/cos lat2))
-        x (- (* (Math/cos lat1) (Math/sin lat2))
-             (* (Math/sin lat1) (Math/cos lat2) (Math/cos delta-lon)))]
-    (mod (+ (Math/toDegrees (Math/atan2 y x)) 360)
+        y (* (Math/sin Δλ) (Math/cos φ2))
+        x (- (* (Math/cos φ1) (Math/sin φ2))
+             (* (Math/sin φ1) (Math/cos φ2) (Math/cos Δλ)))]
+    (rem (+ (Math/toDegrees (Math/atan2 y x)) 360)
          360)));
