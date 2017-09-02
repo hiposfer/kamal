a26921694e17ab6add9ba5e839b088927d026815
carocad <carocad@unal.edu.co>
2017-09-02 15:29:23 +0200

    prefer namespace qualified keywords

--- src/service/routing/graph/specs.clj
+++ src/service/routing/graph/specs.clj
@@ -3,20 +3,18 @@
             [clojure.spec.alpha :as s]
             [service.routing.graph.protocols :as rp]))
 
-(s/def :arc/way int?)
-(s/def :arc/dst int?)
-(s/def :arc/src int?)
+(s/def ::way int?)
+(s/def ::dst int?)
+(s/def ::src int?)
 
-(s/def :graph/arc (s/and (s/keys :req-un [:arc/src :arc/dst :arc/way])
-                         (fn [arc] (not= (:src arc) (:dst arc)))))
+(s/def ::arc (s/keys :req-un [::src ::dst ::way]))
 
-(s/def :node/lat (s/and number? #(<= -90 % 90)))
-(s/def :node/lon (s/and number? #(<= -180 % 180)))
-(s/def :node/arcs (s/map-of int? :graph/arc))
+(s/def ::lat (s/and number? #(<= -90 % 90)))
+(s/def ::lon (s/and number? #(<= -180 % 180)))
+(s/def ::arcs (s/map-of int? ::arc))
 
-(s/def :graph/node (s/and (s/keys :req-un [:node/lat :node/lon :node/arcs])
-                          #(not-empty (:arcs %))))
-(s/def :int-map/graph (s/map-of int? :graph/node))
+(s/def ::node (s/keys :req-un [::lat ::lon ::arcs]))
+(s/def ::graph (s/map-of int? ::node))
 
 ;; todo: also need to validate src id since it can be reversed
 (defn- valid-arcs?
@@ -39,6 +37,6 @@
   :int-map/graph as the base check"
   [graph]
   (let [ids (set (keys graph))]
-    (s/and :int-map/graph
+    (s/and ::graph
            (partial valid-ids? ids)
            valid-arcs?)))
