3a01a3c346756ea711c69ee53020410b8126af69
carocad <carocad@unal.edu.co>
2017-09-03 15:48:52 +0200

    first sketch of Routing App as components drop lein ring add jetty adapter

--- project.clj
+++ project.clj
@@ -7,24 +7,25 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/data.xml "0.0.8"] ; parse xml lazily
                  [org.clojure/data.int-map "0.2.4"] ; fast integers-map
+                 [org.clojure/test.check "0.9.0"] ;; generators
                  [metosin/compojure-api "2.0.0-alpha7"]
                  [metosin/spec-tools "0.3.3"]
-                 [org.clojure/test.check "0.9.0"]
-                 [org.apache.commons/commons-compress "1.4"]]
+                 [ring/ring-jetty-adapter "1.6.2"]
+                 [com.stuartsierra/component "0.3.2"]
+                 [org.apache.commons/commons-compress "1.4"]] ;;bz2 files read
   :profiles {:dev {:dependencies [[criterium "0.4.4"]  ;; benchmark
                                   [expound "0.1.1"]
                                   [io.aviso/pretty "0.1.34"]]}}
   :uberjar-name "routing.jar"
+  :main service.routing.core
+  :aot [service.routing.core] ;; compile the entry point and all of its dependencies
   :test-selectors {:default (complement :benchmark)
                    :benchmark :benchmark}
   :global-vars {*warn-on-reflection* true}
   ;;FIXME: https://github.com/technomancy/leiningen/issues/2173
   :monkeypatch-clojure-test false
   :plugins [[jonase/eastwood "0.2.3"]
-            [lein-ring "0.12.0"]
             [io.aviso/pretty "0.1.34"]]
-  :ring {:handler service.routing.core/app
-         :auto-reload? true}
   :jvm-opts ["-Xmx500m" "-XX:-OmitStackTraceInFastThrow"])
   ;; "-Dclojure.compiler.direct-linking=true"
   ;; https://github.com/clojure/clojure/blob/master/changes.md#11-direct-linking
--- src/service/routing/core.clj
+++ src/service/routing/core.clj
@@ -1,12 +1,15 @@
 (ns service.routing.core
+  (:gen-class)
   (:require [clojure.spec.gen.alpha :as gen]
             [ring.util.http-response :refer [ok]]
-            [compojure.api.sweet :refer [context GET defapi]]
+            [compojure.api.sweet :refer [context GET api]]
             [service.routing.spec :as spec]
             [service.routing.directions :as dir]
             [service.routing.graph.generators :as g]
             [clojure.string :as str]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn]
+            [com.stuartsierra.component :as component]
+            [ring.adapter.jetty :as jetty]))
             ;[expound.alpha :as expound]
 
 (defn- parse-coordinates
@@ -22,29 +25,55 @@
 ;(->radiuses "1200.50;100;500;unlimited;100")
 
 
-(defapi app
-  {:swagger {:ui "/"
-             :spec "/swagger.json"
-             :data {:info {:title "Routing API"
-                           :description "Routing for hippos"}
-                    :tags [{:name "direction", :description "direction similar to mabbox"}]}}}
-  (GET "/spec/direction/:coordinates" []
-    :coercion :spec
-    :summary "direction with clojure.spec"
-    :path-params [coordinates :- ::spec/raw-coordinates]
-    :query-params [{steps :- boolean? false}
-                   {radiuses :- ::spec/raw-radiuses nil}
-                   {alternatives :- boolean? false}
-                   {language :- string? "en"}]
-    :return ::spec/direction
-    (ok (let [coordinates (parse-coordinates coordinates)
-              radiuses    (some-> radiuses (parse-radiuses))]
-          (if (and (not-empty radiuses) (not= (count radiuses) (count coordinates)))
-            {:message "The same amount of radiouses and coordinates must be provided"
-             :code    "InvalidInput"}
-            (dir/direction (g/complete (gen/generate (g/graph 1000)))
-              :coordinates coordinates
-              :steps steps
-              :radiuses radiuses
-              :alternatives alternatives
-              :language language))))))
+(def app
+  (api {:swagger {:ui "/"
+                  :spec "/swagger.json"
+                  :data {:info {:title "Routing API"
+                                :description "Routing for hippos"}
+                         :tags [{:name "direction", :description "direction similar to mabbox"}]}}}
+    (GET "/spec/direction/:coordinates" []
+      :coercion :spec
+      :summary "direction with clojure.spec"
+      :path-params [coordinates :- ::spec/raw-coordinates]
+      :query-params [{steps :- boolean? false}
+                     {radiuses :- ::spec/raw-radiuses nil}
+                     {alternatives :- boolean? false}
+                     {language :- string? "en"}]
+      :return ::spec/direction
+      (ok (let [coordinates (parse-coordinates coordinates)
+                radiuses    (some-> radiuses (parse-radiuses))]
+            (if (and (not-empty radiuses) (not= (count radiuses) (count coordinates)))
+              {:message "The same amount of radiouses and coordinates must be provided"
+               :code    "InvalidInput"}
+              (dir/direction (g/complete (gen/generate (g/graph 1000)))
+                :coordinates coordinates
+                :steps steps
+                :radiuses radiuses
+                :alternatives alternatives
+                :language language)))))))
+
+(defrecord AppServer [port server]
+  component/Lifecycle
+  (start [component]
+    (println ";; Starting App server")
+    (let [server (jetty/run-jetty app {:port port})] ;:join? false})]
+      (assoc component :server server)))
+  (stop [component]
+    (println ";; Stopping App server")
+    (.stop server)
+    component))
+
+(defn system [config-options]
+  (-> (component/system-map
+        ;:config-options config-options
+        ;:db (new-database host port)
+        ;:sched (new-scheduler)
+        :app (map->AppServer config-options))))
+      ;(component/system-using
+      ;   {:app {:database  :db}
+      ;          :scheduler :sched)))))
+
+(defn -main [& args]
+  (println "Welcome to my project! These are your args:" args)
+  (let [[port] args]
+    (component/start (system {:port 8080}))))
