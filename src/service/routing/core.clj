b9e97cd3ef8e141f13685653d84ab1f1718f7476
Camilo Roca <carocad@unal.edu.co>
2017-09-08 14:34:14 +0200

    Split app into swappable components (#45)
    * first sketch of Routing App as components
    drop lein ring
    add jetty adapter
    
    * fix: only AOT when using uberjar
    
    * splitted app into:
    - dev only for reloaded workflow
    - server only for ApiServer related config
    - core entry point of the app
    
    * fix: docstring in wrong position
    
    * fix: allow "lein run" without creating an uberjar
    
    * fix: all env vars are strings
    
    * add default config
    
    * create config maps on the fly
    
    * ignore nil values on -main args
    
    * fix build command
    
    * add some spacing for better readability
    
    * cosmetic changes

--- src/service/routing/core.clj
+++ src/service/routing/core.clj
@@ -1,50 +1,56 @@
 (ns service.routing.core
-  (:require [clojure.spec.gen.alpha :as gen]
-            [ring.util.http-response :refer [ok]]
-            [compojure.api.sweet :refer [context GET defapi]]
-            [service.routing.spec :as spec]
-            [service.routing.directions :as dir]
-            [service.routing.graph.generators :as g]
-            [clojure.string :as str]
-            [clojure.edn :as edn]))
-            ;[expound.alpha :as expound]
+  (:gen-class)
+  (:require [clojure.edn :as edn]
+            [environ.core :refer [env]]
+            [ring.adapter.jetty :as jetty]
+            [com.stuartsierra.component :as component]
+            [service.routing.server :as server])
+  (:import (org.eclipse.jetty.server Server)))
 
-(defn- parse-coordinates
-  [text]
-  (let [pairs (str/split text #";")]
-    (for [coords pairs]
-      (mapv edn/read-string (str/split coords #",")))))
-;(->coordinates "-122.42,37.78;-77.03,38.91")
+(defrecord AppServer [handler options]
+  component/Lifecycle
+  (start [component]
+    (if (:server component)
+      component
+      (let [server (jetty/run-jetty handler options)]
+        (println ";; Starting App server")
+        (assoc component :server server))))
+  (stop [component]
+    (if-let [server (:server component)]
+      (do (println ";; Stopping App server")
+          (.stop ^Server server)
+          (.join ^Server server)
+          (assoc component :server nil)))
+    component))
 
-(defn- parse-radiuses
-  [text]
-  (map edn/read-string (str/split text #";")))
-;(->radiuses "1200.50;100;500;unlimited;100")
+(defn system [config-options]
+  (component/system-map
+    :config-options config-options
+    ;:db (new-database host port)
+    :app (map->AppServer {:handler server/app :options config-options})))
+  ;(component/system-using
+  ;   {:app {:database  :db}
+  ;          :scheduler :sched)))))
 
+(defn config
+  "returns a configuration for the system based on defaults, environment
+  variables and custom options. The configuration will be merged in that
+  order. Returns a configuration map for starting a system"
+  ([] (config {}))
+  ([custom-options]
+   (let [default  (edn/read-string (slurp "resources/default-config.edn"))
+         env-opts {:port     (edn/read-string (env :port))
+                   :join?    (edn/read-string (env :join?))
+                   :osm-file (env :osm-file)}]
+     (merge default
+            (into {} (remove (comp nil? second) env-opts)) ;; ignore nil values
+            custom-options))))
 
-(defapi app
-  {:swagger {:ui "/"
-             :spec "/swagger.json"
-             :data {:info {:title "Routing API"
-                           :description "Routing for hippos"}
-                    :tags [{:name "direction", :description "direction similar to mabbox"}]}}}
-  (GET "/spec/direction/:coordinates" []
-    :coercion :spec
-    :summary "direction with clojure.spec"
-    :path-params [coordinates :- ::spec/raw-coordinates]
-    :query-params [{steps :- boolean? false}
-                   {radiuses :- ::spec/raw-radiuses nil}
-                   {alternatives :- boolean? false}
-                   {language :- string? "en"}]
-    :return ::spec/direction
-    (ok (let [coordinates (parse-coordinates coordinates)
-              radiuses    (some-> radiuses (parse-radiuses))]
-          (if (and (not-empty radiuses) (not= (count radiuses) (count coordinates)))
-            {:message "The same amount of radiouses and coordinates must be provided"
-             :code    "InvalidInput"}
-            (dir/direction (g/complete (gen/generate (g/graph 1000)))
-              :coordinates coordinates
-              :steps steps
-              :radiuses radiuses
-              :alternatives alternatives
-              :language language))))))
+(defn -main [& args]
+  (println "\n\nWelcome to the org.n7a235/service.routing App")
+  (let [config     (config)
+        port       (edn/read-string (first args))
+        overwrites (into {} (remove (comp nil? second))
+                            {:port port}) ;; ignore nil values
+        new-config (merge config overwrites)]
+    (component/start (system new-config))))
