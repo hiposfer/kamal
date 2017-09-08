ef3d5cc7d75b2d976e1b17751f60a5635450c515
Camilo Roca <carocad@unal.edu.co>
2017-09-08 14:34:14 +0200

    Split app into swappable components (#45)
    * first sketch of Routing App as components
    drop lein ring
    add jetty adapter
    
    * fix: only AOT when using uberjar
    
    * splitted app into:
    - dev only for reloaded workflow
    - server only for ApiServer related config
    - core entry point of the app
    
    * fix: docstring in wrong position
    
    * fix: allow "lein run" without creating an uberjar
    
    * fix: all env vars are strings
    
    * add default config
    
    * create config maps on the fly
    
    * ignore nil values on -main args
    
    * fix build command
    
    * add some spacing for better readability
    
    * cosmetic changes
    
    
    Former-commit-id: b9e97cd3ef8e141f13685653d84ab1f1718f7476

---
+++ src/service/routing/dev.clj
@@ -0,0 +1,44 @@
+(ns service.routing.dev
+  "Tools for interactive development with the REPL. This file should
+  not be included in a production build of the application."
+  (:require [com.stuartsierra.component :as component]
+            [environ.core :refer [env]]
+            [service.routing.core :as routing]
+            [clojure.tools.namespace.repl :as repl]))
+
+(def system nil)
+
+(defn init!
+  "Constructs the current development system."
+  []
+  (alter-var-root #'system
+    (constantly (routing/system (routing/config)))))
+
+(defn start!
+  "Starts the current development system."
+  []
+  (alter-var-root #'system component/start))
+
+(defn stop!
+  "Shuts down and destroys the current development system."
+  []
+  (alter-var-root #'system (fn [s] (when s (component/stop s)))))
+
+(defn go!
+  "Initializes the current development system and starts it running."
+  []
+  (when system ;; prevent leaking resources if already started
+    (stop!))
+  (init!)
+  (start!))
+
+;; WARN: this will fail if you just ran `lein uberjar` without
+;; cleaning afterwards. See
+;; https://stackoverflow.com/questions/44246924/clojure-tools-namespace-refresh-fails-with-no-namespace-foo
+(defn reset
+  "reset the system to a fresh state. Prefer using this over go!"
+  []
+  (stop!)
+  (repl/refresh :after 'service.routing.dev/go!))
+
+;(reset)
