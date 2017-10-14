a88296740e81fd86152a3f3dc5f4f34c84965da3
Camilo Roca <carocad@unal.edu.co>
2017-10-14 12:58:18 +0200

    nodes, edges, predecessors and successors functions created to complete graph API (#78)
    * fix: ::nodes added to fake ways
    Queryable protocol removed
    
    * added neighbours to complete function
    renamed position comparator to lexicographic-coordinate
    
    * fix: typos
    
    * version bumped

--- src/hiposfer/kamal/graph/generators.clj
+++ src/hiposfer/kamal/graph/generators.clj
@@ -4,8 +4,11 @@
             [clojure.string :as str]
             [hiposfer.kamal.graph.protocols :as rp]
             [clojure.spec.alpha :as s]
-            [hiposfer.kamal.graph.core :as route]
-            [clojure.data.int-map :as imap])) ;; loads the spec in the registry
+            [hiposfer.kamal.graph.core :as graph]
+            [clojure.data.int-map :as imap]
+            [clojure.data.avl :as avl]
+            [clojure.set :as set]
+            [hiposfer.kamal.libs.math :as math]))
 
 (defn- grapher
   "returns a graph based on the provided node ids. Random latitude and longitudes
@@ -14,16 +17,15 @@
   (let [pick     #(rand-nth (seq ids))
         lat-gen   (partial gen/generate (s/gen ::geojson/lat))
         lon-gen   (partial gen/generate (s/gen ::geojson/lon))
-        arcer    #(route/->Edge (pick) (pick) (rand-int (* 3 (count ids))))
+        arcer    #(graph/->Edge (pick) (pick) (rand-int (* 3 (count ids))))
         ;; create 3 times as many edges as node IDs
         edges     (repeatedly (* 3 (count ids)) arcer)
         ;; first create nodes to store the edges
-        graph     (into (imap/int-map) (map #(vector % (route/->NodeInfo (lat-gen)
+        nodes     (into (imap/int-map) (map #(vector % (graph/->NodeInfo (lat-gen)
                                                                          (lon-gen)
                                                                          nil nil)))
                         ids)]
-    ;; now connect the nodes
-    (reduce rp/connect graph edges)))
+    (reduce rp/connect nodes edges)))     ;; now connect the nodes
 
 (defn graph
   "returns a graph generator with node's id between 0 and 3*size.
@@ -50,10 +52,15 @@
         ;; An string 90% of the time, nil 10%
         namer   (gen/frequency [[1 (gen/return nil)]
                                 [9 (gen/fmap str/capitalize string-alpha)]])
-        ways    (map (fn [id] [id {:name (gen/generate namer)}])
+        ways    (map (fn [id] [id {:name (gen/generate namer)
+                                   :hiposfer.kamal.osm/nodes (into [] (comp (filter #(= id (rp/way %)))
+                                                                            (map rp/src))
+                                                                      (graph/edges graph))}])
                      way-ids)]
-    {:graph graph
-     :ways  (into {} ways)}))
+    {:graph      graph
+     :ways       (into {} ways)
+     :neighbours (into (avl/sorted-map-by math/lexicographic-coordinate)
+                       (set/map-invert graph))}))
 
 ;; example usage
 ;(complete (gen/generate (graph 10)))
