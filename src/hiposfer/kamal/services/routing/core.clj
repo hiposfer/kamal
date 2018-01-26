efb299863fb5d61de9ffe8fdd2b9a872eac4e934
Camilo Roca <camilo.roca@cobi.bike>
2018-01-26 14:52:57 +0100

    refactored configuration file looking into the future refactored routing service to use records and a map of networks removed environ dependency use Java's system env rename reset to refresh

--- src/hiposfer/kamal/services/routing/core.clj
+++ src/hiposfer/kamal/services/routing/core.clj
@@ -1,18 +1,35 @@
 (ns hiposfer.kamal.services.routing.core
   (:require [hiposfer.kamal.parsers.osm :as osm]
             [hiposfer.kamal.graph.generators :as g]
-            [clojure.test.check.generators :as gen]))
+            [clojure.test.check.generators :as gen]
+            [com.stuartsierra.component :as component]))
 
 (def network (comp osm/neighbourhood osm/network))
 
 (def fake-network (comp osm/neighbourhood g/complete gen/generate g/graph))
 
-(defn service
+(defn start!
   [config]
-  (let [result (agent nil)
-        size   (or (:network-size config) 1000)
-        url    (:osm_url config)
-        exec!  (if (:dev config) #(time (fake-network size))
-                                 #(time (network url)))]
-    (println "-- Starting road network with" config)
-    (send-off result exec!)))
+  (let [result (zipmap (keys (:networks config))
+                       (map agent (vals (:networks config))))
+        exec   (if (:dev config) #(time (fake-network (:size %)))
+                                 #(time (network (:osm %))))]
+    (run! (fn [ag] (send-off ag exec))
+          (vals result))
+    result))
+
+(defrecord Router [config networks]
+  component/Lifecycle
+  (start [this]
+    (if (:networks this) this
+      (do (println "-- starting Router with" (keys (:networks config)))
+          (newline)
+          (assoc this :networks (start! config)))))
+  (stop [this]
+    (println "-- stopping Router")
+    (->Router (:config this) nil)))
+
+(defn service
+  "returns a Router record that will contain the config
+   of and all the networks of the system as agents"
+  [] (map->Router {}))
