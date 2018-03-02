ac0ec1f561e040fa8d58de41e831639d604c7cbe
carocad <carocad@unal.edu.co>
2018-03-02 19:45:30 +0100

    fix: removed unused methods fix: removed time-rebasing and use midnight time instead fix: group stop_times only once added NOTES about implementation details fix: remove unused stops fix: added stops to graph

--- src/hiposfer/kamal/parsers/gtfs/core.clj
+++ src/hiposfer/kamal/parsers/gtfs/core.clj
@@ -3,9 +3,9 @@
             [hiposfer.kamal.libs.tool :as tool]
             [hiposfer.kamal.network.graph.protocols :as gp]
             [clojure.data.int-map :as imap]
-            [hiposfer.kamal.parsers.gtfs.preprocessor :as pregtfs]
-            [clojure.data.priority-map :as mprio])
-  (:import (java.time DayOfWeek LocalTime Duration LocalDateTime)))
+            [clojure.data.priority-map :as mprio]
+            [hiposfer.kamal.network.graph.core :as graph])
+  (:import (java.time DayOfWeek)))
 
 (def days {:monday    DayOfWeek/MONDAY
            :tuesday   DayOfWeek/TUESDAY
@@ -25,28 +25,13 @@
      (merge (select-keys calendar [:start_date :end_date])
             {:days (reduce-kv rf #{} (select-keys calendar (keys days)))})]))
 
-;(fuse @(first (:networks (:router hiposfer.kamal.dev/system)))
-;       (parsedir "resources/gtfs/"))))
-
-;; detailed diagram of files relations
-;; http://tommaps.com/wp-content/uploads/2016/09/gtfs-feed-diagram.png
-
-;; Route Planning in Transportation Networks
-;; Overview of the different algorithms and models used
-;; - https://arxiv.org/pdf/1504.05140.pdf
-
-(defn start-time
-  "computes the start LocalTime of a trip based on the Duration instances
-  of stop_times"
-  [stop_times]
-  (LocalTime/ofSecondOfDay (:arrival_time (first stop_times))))
-
 ;; trips is a map of trip_id to a map of
 ;- :src/arrival_time -> the time it takes the vehicle to arrive this stop
+;                       measured from midnight
 ;- :src/departure_time -> the time it takes the vehicle to depart from this
-;                         stop
+;                         stop measured from midnight
 ;- :dst/arrival_time -> the time it takes the vehicle to arrive at the next
-;                       stop
+;                       stop measured from midnight
 (defrecord Connection [^Long src ^Long dst trips]
   gp/Link
   (src [_] src)
@@ -58,17 +43,17 @@
 (defn prepare-connections
   "transforms a stop_times sequence into a sequence of Connections"
   [stop_times]
-  (let [begin (:arrival_time (first stop_times))]
-    (map (fn [s1 s2]
-           (map->Connection
-             {:src (:stop_id s1)
-              :dst (:stop_id s2)
-              :trips (mprio/priority-map-keyfn :src/departure_time
-                       (:trip_id s1)
-                       {:src/arrival_time   (- (:arrival_time s1) begin)
-                        :src/departure_time (- (:departure_time s1) begin)
-                        :dst/arrival_time   (- (:arrival_time s2) begin)})}))
-         stop_times (rest stop_times))))
+  (map (fn [s1 s2]
+         (map->Connection
+           {:src (:stop_id s1)
+            :dst (:stop_id s2)
+            :trips (mprio/priority-map-keyfn :src/departure_time
+                     (:trip_id s1)
+                     {:src/arrival_time   (:arrival_time s1)
+                      :src/departure_time (:departure_time s1)
+                      :dst/arrival_time   (:arrival_time s2)})}))
+       stop_times
+       (rest stop_times)))
 
 (defn- route-for-conn
   "takes a connection with a single trip and returns its route id"
@@ -82,9 +67,11 @@
 
 (defn- connections
   "returns a sequence of [stop_id [route_id Connection]]"
-  [gtfs]
-  (let [trips       (group-by :trip_id (:stop_times gtfs))
-        connections (sequence (comp (map val) (map prepare-connections) cat) trips)]
+  [gtfs trips]
+  (let [connections (sequence (comp (map val)
+                                    (map prepare-connections)
+                                    cat)
+                              trips)]
     (for [[stop sconns] (group-by gp/src connections)
           [route conns] (group-by #(route-for-conn % gtfs) sconns)]
       [stop [route (apply merge-with connector conns)]])))
@@ -95,19 +82,17 @@
                                             :lat (:stop_lat stop)
                                             :name (:stop_name stop)}))
 
-(defn hash-gtfs
+(defn gtfs-by-id
   "transforms the GTFS calendar, stops, trips and route entries
   into a map structure with the entry id as key and the rest as
   value"
   [gtfs]
-  (let [nodes    (into (imap/int-map) (comp (mapize :stop_id)
-                                            (tool/map-vals xnode))
-                       (:stops gtfs))
-        trips    (into {} (mapize :trip_id) (:trips gtfs))
-        routes   (into (imap/int-map) (mapize :route_id) (:routes gtfs))
-        calendar (into {} (map service) (:calendar gtfs))]
-    (assoc gtfs :stops nodes :trips trips
-                :routes routes :calendar calendar)))
+  (assoc gtfs
+    :stops    (into (imap/int-map) (comp (mapize :stop_id) (tool/map-vals xnode))
+                    (:stops gtfs))
+    :trips    (into {} (mapize :trip_id) (:trips gtfs))
+    :routes   (into (imap/int-map) (mapize :route_id) (:routes gtfs))
+    :calendar (into {} (map service) (:calendar gtfs))))
 
 (comment for a connection store a set of
          {:trip_id 1
@@ -121,12 +106,13 @@
 
 (comment
   - if a traversal comes with no trip
-    for every trip in the connection
-      compute :src/departure_time + :trip/start_time
-      if current_time < total_time
-        take that trip -> return the {:duration :trip_id}
-    otherwise
-      there is no trip -> Double/POSITIVE_INFINITY
+    - remove the trips that dont drive that day
+    - for every trip in the connection
+        compute :src/departure_time + :user/date
+        if current_time < total_time
+          take that trip -> return the {:duration :trip_id}
+      otherwise
+        there is no trip -> Double/POSITIVE_INFINITY
   - if a traversal comes with a trip_id
     find the trip with that id and return the duration)
 
@@ -138,35 +124,55 @@
 ;;   from individual stops for each trip
 ;; - a Stop is an Individual locations where vehicles pick up or drop off passengers
 
-;; Base on the previous information the fusion of network and gtfs can be done as
-;; follows:
-;; - map the stop ID to node ID
-;; - create a :gtfs/routes entry in the network to contain the metadata
-;; - group the stop_times by trip_id
-;; - for each consecutive stops_id in a trip
-;;   - create a [src dst] tuple
-;;   - group them by src node
-;;     - group those by route_id
-;;     - combine/reduce the tuple into a single connection
-;;       - this will behave as a time-dependent arc
-;;       - it will contain a sorted set with the :src/arrival_time as key
-;;       - while traversing a simple (avl/nearest > ...) should be enough
-;;         to get the corresponding connection
-;;       - neither the stop_id nor the stop_sequence are needed inside the connection
+;; detailed diagram of files relations
+;; http://tommaps.com/wp-content/uploads/2016/09/gtfs-feed-diagram.png
 
+;; Route Planning in Transportation Networks
+;; Overview of the different algorithms and models used
+;; - https://arxiv.org/pdf/1504.05140.pdf
+
+;; A good introduction to time-dependent model for timetable information
+;; - https://www.ceid.upatras.gr/webpages/faculty/zaro/pub/jou/J23-TTI-Springer.pdf
+
+(defn- node-merger
+  "reducer function to merge a graph with a gtfs stops without losing information"
+  [graph [id stop]]
+  (if (contains? graph id)
+    (update graph id merge stop)
+    (assoc graph id stop)))
+
+;; NOTE: we wont model the vehicle stations as mini-graph as is proposed in most
+;; articles. Although it makes the model clearer from a graph theory perspective
+;; it also makes it more difficult to maintain, specially considering real-time
+;; updates. For example: to change the transfer time between two routes at a
+;; specific station, you would have to find out that station, then find out
+;; the arc connecting those and update it. As if that wasnt enough, you need to
+;; create fake nodes for the route stops (which id do you use? how do you ensure
+;; that it is unique or that no collisions will occur in the future?)
+
+;; Instead of that we will simply have a base penalty whenever you transfer from
+;; a route to another. If there is a special connection between two routes then
+;; it can be stored in a :gtfs/transfers structure which can be easily lookup
 (defn fuse
   "fuses the content of the gtfs data with the network"
-  [network hashed-gtfs]
-  (let [conns (into {} (connections hashed-gtfs))
-        trips (group-by :trip_id (:stop_times hashed-gtfs))]
-    (take 10 trips)))
+  [network mgtfs]
+  (let [trips     (group-by :trip_id (:stop_times mgtfs))
+        conns     (connections mgtfs trips)
+        removable (imap/difference (imap/int-set (keys (:stops mgtfs)))
+                                   (imap/int-set (map first conns)))
+        stops     (apply dissoc (:stops mgtfs) removable)
+        graph     (reduce node-merger (:graph network) stops)]
+    ;; TODO: link nodes with routes
+    (map second conns)))
 
-;(def foo (time (hash-gtfs (pregtfs/parsedir "resources/gtfs/"))))
-
+;(def foo (time (gtfs-by-id (pregtfs/parsedir "resources/gtfs/"))))
 ;(keys foo)
-;(take 5 (:routes foo))
+;(def network (keys @(first (:networks (:router hiposfer.kamal.dev/system)))))
 
-;(take 2 (connections foo))
+;(take 2 (fuse network foo))
+
+;(fuse @(first (:networks (:router hiposfer.kamal.dev/system)))
+;       (parsedir "resources/gtfs/"))))
 
 ;(prepare-connections (second (first (group-by :trip_id (:stop_times foo)))))
 
