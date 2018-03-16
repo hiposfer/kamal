4381785156daaa8d9befa73a0611625577b076b7
Camilo Roca <camilo.roca@cobi.bike>
2018-03-16 12:15:29 +0100

    refactored upcoming trip for speed refactored continue trip to return the first element
    Former-commit-id: 0ee9fff1dd9ae4d3219f50d0cbf626e8ae63037d

--- src/hiposfer/kamal/libs/fastq.clj
+++ src/hiposfer/kamal/libs/fastq.clj
@@ -2,7 +2,14 @@
   "namespace for hand-optimized queries that are used inside the routing
   algorithm and need to run extremely fast (< 1 ms per query)"
   (:require [datascript.core :as data])
-  (:import (java.time LocalDateTime LocalDate LocalTime)))
+  (:import (java.time LocalDateTime LocalDate LocalTime Duration)))
+
+(defn index-lookup
+  "convenience function to get the entities whose attribute (k) equals id"
+  [network k id]
+  (eduction (comp (take-while #(= (:v %) id))
+                  (map #(data/entity network (:e %))))
+            (data/index-range network k id nil)))
 
 (defn node-successors
   "takes a network and an entity id and returns the successors of that entity.
@@ -19,13 +26,12 @@
   takes around 0.25 milliseconds"
   [network id]
   (concat (map :db/id (:node/successors (data/entity network id)))
-          (eduction (comp (map :e) (take-while #(= (:v %) id)))
-                    (data/index-range network :node/successors id nil))))
+          (map :db/id (index-lookup network :node/successors id))))
 
 (defn nearest-node
   "returns the nearest node/location datom to point"
   [network point]
-  (first (data/index-range network :node/location point nil)))
+  (first (index-lookup network :node/location point)))
 
 (defn node-ways
   "takes a dereferenced Datascript connection and an entity id and returns
@@ -39,18 +45,11 @@
   The previous query takes around 50 milliseconds to finish. This one takes
   around 0.15 milliseconds"
   [network id]
-  (take-while #(= (:v %) id) (data/index-range network :way/nodes id nil)))
+  (index-lookup network :way/nodes id))
 
 ;; utility functions to avoid Datascript complaining about it
 (defn plus-seconds [^LocalDateTime t amount] (.plusSeconds t amount))
 (defn after? [^LocalDateTime t ^LocalDateTime t2] (.isAfter t t2))
-
-(defn index-lookup
-  "convenience function to get the entities whose attribute (k) equals id"
-  [network k id]
-  (eduction (comp (take-while #(= (:v %) id))
-                  (map #(data/entity network (:e %))))
-            (data/index-range network k id nil)))
 
 (defn- continue-xform
   "for some reason, defining the transducer outside of the eduction itself
@@ -61,7 +60,7 @@
         (map #(plus-seconds ?start %))))
 
 (defn continue-trip
-  "find the time of arrival for a certain trip to a specifc stop
+  "find the time of arrival for a certain trip to a specific stop
 
   replaces:
   '[:find ?departure
@@ -72,20 +71,10 @@
            [(plus-seconds ?start ?seconds) ?departure]]
 
    The previous query takes around 50 milliseconds to execute. This function
-   takes around 0.22 milliseconds to execute. Depends on :stop.times/trip index.
-   "
+   takes around 0.22 milliseconds to execute. Depends on :stop.times/trip index"
   [network ?dst-id ?trip ?start]
-  (eduction (continue-xform ?dst-id ?start)
-            (index-lookup network :stop.times/trip ?trip)))
-
-
-(defn- xf
-  "reducing function for upcoming-trip. Just for convenience"
-  ([] nil)
-  ([res v]
-   (if (pos? (compare (:stop.times/departure_time res) (:stop.times/departure_time v)))
-     v
-     res)))
+  (first (eduction (continue-xform ?dst-id ?start)
+                   (index-lookup network :stop.times/trip ?trip))))
 
 ;; TODO: ideally these computations can be cached to improve performance
 ;; to avoid cache misses I guess the ideal way would be to cache the trip
@@ -118,15 +107,12 @@
 
   The previous query runs in 118 milliseconds. This function takes 4 milliseconds"
   [network ?src-id ?dst-id ?now ?start]
-  (let [sources (index-lookup network :stop.times/stop ?src-id)
-        dests   (index-lookup network :stop.times/stop ?dst-id)
-        four    (into {} (map #(vector (:stop.times/trip %) %)) dests)
-        trips   (filter #(contains? four (:stop.times/trip %)) sources)
-        stime   (reduce xf (for [trip trips
-                                 :let [departs (plus-seconds ?start (:stop.times/departure_time trip))]
-                                 :when (after? departs ?now)]
-                             trip))]
-    (get four (:stop.times/trip stime))))
+  (let [t    (.getSeconds (Duration/between ?start ?now))
+        all  (data/index-range network :stop.times/departure_time t nil)
+        src  (first (filter #(= ?src-id (:db/id (:stop.times/stop %)))
+                            (map #(data/entity network (:e %)) all)))
+        dst-arrival (continue-trip network ?dst-id (:db/id (:stop.times/trip src)) ?start)]
+    [src dst-arrival]))
 
 ;(time
 ;  (upcoming-trip @(first @(:networks (:router hiposfer.kamal.dev/system)))
@@ -134,7 +120,6 @@
 ;                 230607
 ;                 (LocalDateTime/now)
 ;                 (LocalDateTime/of (LocalDate/now) (LocalTime/MIDNIGHT))))
-
 
 (defn next-stops
   "find the next possible stops of ?src-id based on stop.times
@@ -160,3 +145,6 @@
                          :when (> (:stop.times/sequence st2) (:stop.times/sequence st1))]
                      (:stop.times/stop st2))]
     (distinct stops)))
+
+;(next-stops @(first @(:networks (:router hiposfer.kamal.dev/system)))
+;            230371)
