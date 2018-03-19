397b028ed650074f364f31b99fcf2fe5ae064c99
Camilo Roca <camilo.roca@cobi.bike>
2018-03-20 00:09:55 +0100

    day-trips function added to avoid searching for trips on every datom by-service-day filter removed accordingly fix: only allow trips that apply for the service day index-lookup now returns transducer -> always be composing
    Former-commit-id: 23427437a678c43c3d2df8be8355eaa040823fe7

--- src/hiposfer/kamal/libs/fastq.clj
+++ src/hiposfer/kamal/libs/fastq.clj
@@ -5,14 +5,14 @@
   By convention all queries here return Entities"
   (:require [datascript.core :as data]
             [hiposfer.kamal.libs.tool :as tool])
-  (:import (java.time Duration LocalTime LocalDate)))
+  (:import (java.time Duration LocalTime LocalDate LocalDateTime)))
 
 (defn index-lookup
-  "convenience function to get the entities whose attribute (k) equals id"
-  [network k id]
-  (eduction (comp (take-while #(= (:v %) id))
-                  (map #(data/entity network (:e %))))
-            (data/index-range network k id nil)))
+  "returns a transducer that can be used together with index-range to get all
+  entities attribute k equals id"
+  [network id]
+  (comp (take-while #(= (:v %) id))
+        (map #(data/entity network (:e %)))))
 
 (defn node-successors
   "takes a network and an entity and returns the successors of that entity.
@@ -28,8 +28,11 @@
   The previous query takes around 50 milliseconds to finish. This function
   takes around 0.25 milliseconds"
   [network entity]
-  (concat (:node/successors entity)
-          (index-lookup network :node/successors (:db/id entity))))
+  (let [id (:db/id entity)]
+    (concat (:node/successors entity)
+            (sequence (index-lookup network id)
+                      (data/index-range network :node/successors id nil)))))
+
 
 (defn nearest-node
   "returns the nearest node/location to point"
@@ -49,7 +52,9 @@
   The previous query takes around 50 milliseconds to finish. This one takes
   around 0.15 milliseconds"
   [network entity]
-  (index-lookup network :way/nodes (:db/id entity)))
+  (let [id (:db/id entity)]
+    (sequence (index-lookup network id)
+              (data/index-range network :way/nodes id nil))))
 
 (defn continue-trip
   "returns the :stop.times entity to reach ?dst-id via ?trip
@@ -66,7 +71,8 @@
    takes around 0.22 milliseconds to execute. Depends on :stop.times/trip index"
   [network ?dst-id ?trip]
   (tool/some #(= ?dst-id (:db/id (:stop.times/stop %)))
-              (index-lookup network :stop.times/trip ?trip)))
+             (eduction (index-lookup network ?trip)
+                       (data/index-range network :stop.times/trip ?trip nil))))
 
 (defn- min-departure
   [result value]
@@ -90,11 +96,14 @@
            [(hiposfer.kamal.libs.fastq/after? ?departure ?now)]]
 
   The previous query runs in 118 milliseconds. This function takes 4 milliseconds"
-  [network ?src-id ?dst-id ?now]
-  (when-let [sts  (not-empty (filter #(> (:stop.times/departure_time %) ?now)
-                                      (index-lookup network :stop.times/stop ?src-id)))]
-    (let [trip (reduce min-departure sts)]
-      [trip (continue-trip network ?dst-id (:db/id (:stop.times/trip trip)))])))
+  [network trips ?src-id ?dst-id ?now]
+  (let [sts  (seq (eduction (comp (index-lookup network ?src-id)
+                                  (filter #(and (> (:stop.times/departure_time %) ?now)
+                                                (contains? trips (:db/id (:stop.times/trip %))))))
+                            (data/index-range network :stop.times/stop ?src-id nil)))]
+    (when (seq sts)
+      (let [trip (reduce min-departure sts)]
+        [trip (continue-trip network ?dst-id (:db/id (:stop.times/trip trip)))]))))
 
 ;(time
 ;  (dotimes [n 1000]
@@ -102,12 +111,24 @@
 ;               230963
 ;               230607
 ;               (.getSeconds (Duration/between (LocalTime/MIDNIGHT)
-;                                              (LocalTime/now))))))
+;                                              (LocalTime/now)))))
 
+(defn day-trips
+  "returns a set of trip (entities) ids that are available for date"
+  [network ^LocalDate date]
+  (let [services (into #{} (comp (take-while #(= (:a %) :service/id))
+                                 (map #(data/entity network (:e %)))
+                                 (filter #(and (.isBefore date (:service/end_date %))
+                                               (.isAfter date (:service/start_date %))
+                                               (contains? (:service/days %)
+                                                          (.getDayOfWeek date))))
+                                 (map :db/id))
+                           (data/seek-datoms network :avet :service/id))]
+    (into #{} (comp (take-while #(= (:a %) :trip/service))
+                    (filter #(contains? services (:v %)))
+                    (map :e))
+              (data/seek-datoms network :avet :trip/service))))
 
-;; TODO: this could be cached directly into the stop itself
-;; right now we search all trips for the neighbours which is
-;; very expensive !!
 (defn next-stops
   "return the next stop entities for ?src-id based on stop.times
 
@@ -124,10 +145,13 @@
            [?se :stop/id ?id]]
   the previous query takes 145 milliseconds. This function takes 0.2 milliseconds"
   [network ?src]
-  (let [stop-times (index-lookup network :stop.times/stop (:db/id ?src))
+  (let [id         (:db/id ?src)
+        stop-times (sequence (index-lookup network id)
+                             (data/index-range network :stop.times/stop ?src nil))
         stops      (for [st1  stop-times
-                         :let [trip    (:stop.times/trip st1)
-                               stimes2 (index-lookup network :stop.times/trip (:db/id trip))]
+                         :let [trip-id (:db/id (:stop.times/trip st1))
+                               stimes2 (sequence (index-lookup network trip-id)
+                                                 (data/index-range network :stop.times/trip trip-id nil))]
                          st2  stimes2
                          :when (> (:stop.times/sequence st2) (:stop.times/sequence st1))]
                      (:stop.times/stop st2))]
