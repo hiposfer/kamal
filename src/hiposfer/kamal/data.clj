cdfe482753b5e34462b1abca210970a15dfbe32f
Camilo Roca <camilo.roca@cobi.bike>
2018-05-09 21:15:50 +0200

    edn parser created for datascript data.clj with main created for preprocessing refactored service routing network function to be usable outside of component

--- src/hiposfer/kamal/data.clj
+++ src/hiposfer/kamal/data.clj
@@ -1,51 +1,31 @@
 (ns hiposfer.kamal.data
+  (:gen-class)
   (:require [clojure.java.io :as io]
-            [clojure.edn :as edn]
-            [datascript.core :as data]
             [taoensso.timbre :as timbre]
+            [hiposfer.kamal.services.routing.core :as routing]
             [hiposfer.kamal.core :as core])
-  (:import (java.util.zip ZipOutputStream ZipEntry ZipInputStream)
-           (java.time ZoneId LocalDate DayOfWeek)
-           (java.io PushbackReader InputStreamReader)))
-
+  (:import (java.util.zip GZIPOutputStream)))
 
 (defn- write
-  [routers filename]
-  (with-open [w0 (ZipOutputStream. (io/output-stream filename))
-              w  (io/writer w0)]
-    (let [entry (ZipEntry. "datascript.edn")]
-      (-> w0 (.putNextEntry entry))
-      (binding [*out* w]
-        (pr "foo")))));@(first @(:networks (:router system))))))))
+  "takes a sequence of Datascript atoms and serializes them to
+  a zip"
+  [routers outdir]
+  (doseq [value routers]
+    (let [gfile (str outdir (hash @value) ".gzip")]
+      (with-open [w (io/writer (GZIPOutputStream. (io/output-stream gfile)))]
+        (binding [*out* w]
+          (pr @value))))))
 
-(def java-readers
-  {'java.time.ZoneRegion #(ZoneId/of %)
-   'java.time.LocalDate  #(LocalDate/parse %)
-   'java.time.DayOfWeek  #(DayOfWeek/valueOf %)})
-
-(defn read-object
-  [[package _ value]]
-  (let [parse (get java-readers package)]
-    (parse value)))
-
-(defn- read
-  [filename]
-  (with-open [r0 (ZipInputStream. (io/input-stream filename))
-              r  (PushbackReader. (InputStreamReader. r0))]
-    (.getNextEntry r0)
-    (binding [*read-eval* false]
-      (edn/read {:readers (merge data/data-readers
-                                 {'hiposfer.kamal.network.core.Location hiposfer.kamal.network.core/map->Location
-                                  'object read-object})}
-                r))))
-
-(defn -main [[outfile]]
+;; "resources/data.zip"
+(defn -main
+  "Script for preprocessing OSM and GTFS files into gzip files each with
+  a Datascript EDN representation inside"
+  [[outdir]]
+  (assert (not (nil? outdir)) "missing output file")
   (timbre/info "\n\npreprocessing OSM and GTFS files")
-  (let [config (core/config)]
-    (read "resources/datascript.edn")))
+  (let [config (core/config {:dev false}) ;; just in case
+        result (for [area (:networks config)] (routing/network area))]
+    (write result outdir)))
 
-
-;(data/q '[:find (pull ?id [*])
-;          :where [?id :service/id]
-;          [_ :service/days ?days]]
-;        foo)
+;example
+;(-main ["resources/"])
---
+++ src/hiposfer/kamal/parsers/edn.clj
@@ -0,0 +1,40 @@
+(ns hiposfer.kamal.parsers.edn
+  (:require [clojure.java.io :as io]
+            [clojure.edn :as edn]
+            [datascript.core :as data])
+  (:import (java.time ZoneId LocalDate DayOfWeek)
+           (java.util.zip GZIPInputStream)
+           (java.io PushbackReader InputStreamReader)))
+
+(def java-readers
+  "java class to parsing function. Useful to deserialize things from EDN"
+  {'java.time.ZoneRegion #(ZoneId/of %)
+   'java.time.LocalDate  #(LocalDate/parse %)
+   'java.time.DayOfWeek  #(DayOfWeek/valueOf %)})
+
+(defn read-object
+  "get the appropiate reader for the Java object and parse it. Throws a
+  Null Pointer Exception on unknown class by design !"
+  [[package _ value]]
+  (let [parse (get java-readers package)]
+    (parse value)))
+
+(def local-readers
+  "qualified symbol to parsing function. Used to get datascript to understand local objects"
+  {'hiposfer.kamal.network.core.Location hiposfer.kamal.network.core/map->Location
+   'object                               read-object})
+
+(defn parse
+  "streams a zip file and creates a Datascript DB from each file inside the zip.
+  Returns a set of Datascript DB atoms."
+  [filename]
+  (with-open [stream (-> (io/input-stream filename)
+                         (GZIPInputStream.)
+                         (InputStreamReader.)
+                         (PushbackReader.))]
+    (let [content (edn/read {:readers (merge data/data-readers local-readers)}
+                            stream)]
+      content)))
+
+; example
+;(time (last (parse "/Users/Camilo/Proyectos/kamal/resources/2122204427.gzip")))
