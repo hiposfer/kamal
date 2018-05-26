116eaaf2b6fb5172f91bf97b6640be087884ea2b
Camilo Roca <camilo.roca@cobi.bike>
2018-05-26 15:03:09 +0200

    fix: g1c uses too much memory fix: enable direct linking with datascript latest version fix: update dependencies to latest versions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hiposfer/kamal "0.7.0"
+(defproject hiposfer/kamal "0.7.1"
   :description "An application that provides routing based on external sources and OSM data"
   :url "https://github.com/hiposfer/kamal"
   :license {:name "LGPLv3"
@@ -8,15 +8,15 @@
                  [org.clojure/data.xml "0.0.8"] ; parse xml lazily
                  [org.clojure/test.check "0.9.0"] ;; generators
                  [metosin/compojure-api "2.0.0-alpha18"]
-                 [metosin/spec-tools "0.5.1"] ;;coercion
+                 [metosin/spec-tools "0.7.0"] ;;coercion
                  [hiposfer/geojson.specs "0.2.0"]
                  [com.taoensso/timbre "4.10.0"]
                  [ring/ring-jetty-adapter "1.6.3"]
                  [com.stuartsierra/component "0.3.2"]
-                 [org.apache.commons/commons-compress "1.4"] ;;bz2 files read
+                 [org.apache.commons/commons-compress "1.16.1"] ;;bz2 files read
                  [org.teneighty/java-heaps "1.0.0"]
                  [org.clojure/data.csv "0.1.4"]
-                 [datascript "0.16.4"]
+                 [datascript "0.16.5"]
                  [ch.hsr/geohash "1.3.0"]]
   :aliases {"preprocess" ["trampoline" "run" "-m" "hiposfer.kamal.preprocessor"]}
   :profiles {:dev {:dependencies [[criterium "0.4.4"]  ;; benchmark
@@ -27,8 +27,8 @@
                              [io.aviso/pretty "0.1.34"]]}
              :uberjar {:aot [hiposfer.kamal.core] ;; compile the entry point and all of its dependencies}
                        :main hiposfer.kamal.core
-                       :uberjar-name "kamal.jar"}}
-                       ;:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
+                       :uberjar-name "kamal.jar"
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
   :jar-exclusions [#".*\.bz2"]
   :uberjar-exclusions [#".*\.bz2"]
   :test-selectors {:default (complement :benchmark)
