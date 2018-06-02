6e6277689f97be5f7d2803eb108077652726c3e1
Camilo Roca <camilo.roca@cobi.bike>
2018-06-02 18:59:12 +0200

    fix: type Ok fix: modifier spec fix: accept floats instead of nat int fix: consistency check

--- src/hiposfer/kamal/specs/mapbox/directions.clj
+++ src/hiposfer/kamal/specs/mapbox/directions.clj
@@ -2,33 +2,45 @@
   (:require [clojure.spec.alpha :as s]
             [spec-tools.spec :as spec]
             [hiposfer.geojson.specs :as geojson]
-            [clojure.spec.gen.alpha :as gen])
+            [clojure.spec.gen.alpha :as gen]
+            [hiposfer.kamal.services.routing.directions :as dir])
   (:import (java.time LocalDateTime)))
+
+(defn- consistent?
+  [step]
+  (case (:mode step)
+    "transit" (contains? #{"notification" "continue" "exit vehicle"}
+                         (:type (:maneuver step)))
+    "walking" (contains? #{"depart" "arrive" "turn"}
+                         (:type (:maneuver step)))))
+
+(defn- natural? [n] (>= n 0))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;; RESPONSE
 
 ;; todo ::annotation => https://www.mapbox.com/api-documentation/#routeleg-object
-(s/def ::code        #{"OK" "NoRoute" "NoSegment"})
+(s/def ::code        #{"Ok" "NoRoute" "NoSegment"})
 (s/def ::name        string?)
 (s/def ::summary     string?)
 (s/def ::type        #{"turn" "exit vehicle" "notification" "continue" "depart" "arrive"})
 (s/def ::mode        #{"walking" "transit"})
 (s/def ::instruction (s/and string? not-empty))
-(s/def ::modifier    string?)
+(s/def ::modifier    (set (vals dir/bearing-turns)))
 (s/def ::bearing     (s/and spec/number? #(<= 0 % 360)))
 (s/def ::bearing_before ::bearing)
 (s/def ::bearing_after  ::bearing)
 (s/def ::location    ::geojson/position)
-(s/def ::duration    (s/and spec/number? nat-int?))
-(s/def ::distance    (s/and spec/number? nat-int?))
-(s/def ::weight      (s/and spec/number? nat-int?)) ;; a negative weight doesnt make sense
+(s/def ::duration    (s/and spec/number? natural?))
+(s/def ::distance    (s/and spec/number? natural?))
+(s/def ::weight      (s/and spec/number? natural?)) ;; a negative weight doesnt make sense
 (s/def ::weight_name string?)
 ;; structures
 (s/def ::maneuver    (s/keys :req-un [::location ::bearing_before ::bearing_after
                                       ::instruction ::type]
                              :opt-un [::modifier]))
-(s/def ::route-step  (s/keys :req-un [::distance ::duration ::geometry
-                                      ::name     ::mode     ::maneuver]))
+(s/def ::route-step  (s/and (s/keys :req-un [::distance ::duration ::geometry
+                                             ::name     ::mode     ::maneuver])
+                            consistent?))
 (s/def ::steps       (s/coll-of ::route-step :kind sequential?))
 (s/def ::route-leg   (s/keys :req-un [::distance ::duration ::steps ::summary]))
 (s/def ::legs        (s/coll-of ::route-leg :kind sequential?))
