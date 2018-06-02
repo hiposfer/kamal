c6d8ba68d8e4bae7422c0f5e8522a075512eebcc
Camilo Roca <camilo.roca@cobi.bike>
2018-06-02 19:32:14 +0200

    print time it takes to load network
    Former-commit-id: b6f0670a2937b0517a2b33441a4a5f81b2cf9e11

--- test/hiposfer/kamal/network/tests.clj
+++ test/hiposfer/kamal/network/tests.clj
@@ -184,7 +184,7 @@
 ; -----------------------------------------------------------------
 ; generative tests for the direction endpoint
 
-(def network (delay (router/network {:area/edn "resources/saarland.edn.bz2"})))
+(def network (delay (time (router/network {:area/edn "resources/saarland.edn.bz2"}))))
 
 (defspec saarland-directions
   30; tries -> expensive test
