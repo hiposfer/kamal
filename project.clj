28480561adfdae9d7ee59f9e995d0f7225bee2c5
Camilo Roca <camilo.roca@cobi.bike>
2018-06-21 11:29:20 +0200

    linter config file added fix: linter breaking on spec macros moved global vars to dev ns updated dependencies
    Former-commit-id: b2cce23457b7b9e9a96836af2130e519f8c0c49d

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hiposfer/kamal "0.8.3"
+(defproject hiposfer/kamal "0.9.0"
   :description "An application that provides routing based on external sources and OSM data"
   :url "https://github.com/hiposfer/kamal"
   :license {:name "LGPLv3"
@@ -13,18 +13,19 @@
                  [com.taoensso/timbre "4.10.0"]
                  [ring/ring-jetty-adapter "1.6.3"]
                  [com.stuartsierra/component "0.3.2"]
-                 [org.apache.commons/commons-compress "1.16.1"] ;;bz2 files read
+                 [org.apache.commons/commons-compress "1.17"] ;;bz2 files read
                  [org.teneighty/java-heaps "1.0.0"]
                  [org.clojure/data.csv "0.1.4"]
-                 [datascript "0.16.5"]
+                 [datascript "0.16.6"]
                  [ch.hsr/geohash "1.3.0"]]
   :aliases {"preprocess" ["trampoline" "run" "-m" "hiposfer.kamal.preprocessor"]}
   :profiles {:dev {:dependencies [[criterium "0.4.4"]  ;; benchmark
-                                  [expound "0.6.0"]
+                                  [expound "0.7.0"]
                                   [io.aviso/pretty "0.1.34"]
                                   [org.clojure/tools.namespace "0.2.11"]]
-                   :plugins [[jonase/eastwood "0.2.5"]
-                             [io.aviso/pretty "0.1.34"]]}
+                   :plugins [[jonase/eastwood "0.2.6"]
+                             [io.aviso/pretty "0.1.34"]]
+                   :eastwood {:config-files ["resources/eastwood.clj"]}}
              :release {:aot [hiposfer.kamal.core] ;; compile the entry point and all of its dependencies}
                        :main hiposfer.kamal.core
                        :uberjar-name "kamal.jar"
@@ -33,15 +34,10 @@
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
   :test-selectors {:default (complement :benchmark)
                    :benchmark :benchmark}
-  :global-vars {*warn-on-reflection* true
-                *print-length* 100}
   ;;FIXME: https://github.com/technomancy/leiningen/issues/2173
   :monkeypatch-clojure-test false
-  :jvm-opts ["-Xmx1g" "-XX:-OmitStackTraceInFastThrow"]
   :repositories [["releases"  {:url      "https://clojars.org/repo"
                                :username :env/clojars_username
                                :password :env/clojars_password
                                :sign-releases false}]]
   :deploy-repositories [["releases"  :releases]])
-  ;; "-Dclojure.compiler.direct-linking=true"
-  ;; https://github.com/clojure/clojure/blob/master/changes.md#11-direct-linking
