41d9261f13c7025445a5ec55f4f3e4af0a4534a1
Camilo Roca <camilo.roca@cobi.bike>
2018-08-31 13:37:49 +0200

    fix: file exclusions
    Former-commit-id: 6ff1c54f73ddb796469de8650d9e565e53415500

--- project.clj
+++ project.clj
@@ -30,8 +30,8 @@
              :release {:aot [hiposfer.kamal.core] ;; compile the entry point and all of its dependencies}
                        :main hiposfer.kamal.core
                        :uberjar-name "kamal.jar"
-                       :jar-exclusions [#".*\.bz2"]
-                       :uberjar-exclusions [#".*\.bz2"]
+                       :jar-exclusions [#".*\.gzip" #".*\.zip"]
+                       :uberjar-exclusions [#".*\.gzip" #".*\.zip"]
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
   :test-selectors {:default (complement :benchmark)
                    :benchmark :benchmark}
