ba7898feabf35fd795d5738a20df9dc32b394b28
Camilo Roca <camilo.roca@cobi.bike>
2018-09-28 16:08:58 +0200

    cosmetic changes

--- src/hiposfer/kamal/services/routing/directions.clj
+++ src/hiposfer/kamal/services/routing/directions.clj
@@ -233,8 +233,3 @@
 ;                            [8.635897, 50.104172]]
 ;              :departure (ZonedDateTime/parse "2018-05-07T10:15:30+02:00")
 ;              :steps true}))
-
-;(time
-;  (fastq/day-stop-times @(first @(:networks (:router hiposfer.kamal.dev/system)))
-;                        (. (ZonedDateTime/parse "2018-05-07T10:15:30+02:00")
-;                           (toLocalDate))))
