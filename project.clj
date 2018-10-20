183d53201b272504715542d18ef3fe4837eb130f
Camilo Roca <camilo.roca@cobi.bike>
2018-10-21 01:00:53 +0200

    version bumped

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hiposfer/kamal "0.18.1"
+(defproject hiposfer/kamal "0.19.0"
   :description "An application that provides routing based on external sources and OSM data"
   :url "https://github.com/hiposfer/kamal"
   :license {:name "LGPLv3"
