643fe58d6e001d2cba877ac895d99982553108c4
Camilo Roca <camilo.roca@cobi.bike>
2018-10-21 00:12:42 +0200

    fix: incorrect tag mapping

--- src/hiposfer/kamal/router/io/edn.clj
+++ src/hiposfer/kamal/router/io/edn.clj
@@ -23,8 +23,8 @@
 
 (def local-readers
   "qualified symbol to parsing function. Used to get datascript to understand local objects"
-  {'hiposfer.kamal.router.algorithms.core records/map->Location
-   'object                                           read-object})
+  {'hiposfer.kamal.router.algorithms.core.Location records/map->Location
+   'object                                         read-object})
 
 (defn parse
   "streams a zip file and creates a Datascript DB from each file inside the zip.
