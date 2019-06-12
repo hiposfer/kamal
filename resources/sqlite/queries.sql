f8630a24b4cbc6dcbb6195e203b8275f459f2a29
Camilo Roca <camilo.roca@cobi.bike>
2019-06-13 00:14:37 +0200

    fix: apparently I dont need settled

--- resources/sqlite/queries.sql
+++ resources/sqlite/queries.sql
@@ -8,11 +8,12 @@ select * from link join node as source on link.src = source.id
 -- src 2708331052, dst 561065
 
 with recursive
- path(src, dst, cost, settled) as (
-    values (null, 2708331052, 0, 1)
+ path(src, dst, cost) as (
+    values (null, 2708331052, 0)
     union all
-    select link.src, link.dst, link.distance + path.cost as cost, 0
+    select link.src, link.dst, link.distance + path.cost as cost
      from path join link on link.src = path.dst
      order by cost
  )
-select * from path;
+select * from path
+ -- group by path.src;
