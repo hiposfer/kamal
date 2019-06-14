e0691113d28747f687253669f0831bef2e7dae9a
Camilo Roca <camilo.roca@cobi.bike>
2019-06-14 16:34:27 +0200

    queries for dijkstra algorithm created

--- resources/sqlite/queries.sql
+++ resources/sqlite/queries.sql
@@ -6,14 +6,33 @@ select * from link join node as source on link.src = source.id
                    join node as destination on link.dst = destination.id
 
 -- src 2708331052, dst 561065
-
+-- "sends a radar beacon to know the distance to the destination"
+-- This query is only useful to know the cost of the shortest path
+-- until the destination. We can use it to display it to the user
+-- and as a way to stop the graph_traversal recursive query below
 with recursive
- path(src, dst, cost) as (
+ beacon(src, dst, cost) as (
     values (null, 2708331052, 0)
-    union all
-    select link.src, link.dst, link.distance + path.cost as cost
-     from path join link on link.src = path.dst
-     order by cost
+        union all
+    select link.src, link.dst, link.distance + beacon.cost as cost
+     from beacon
+      join link on link.src = beacon.dst
+      order by cost
+      limit 100000
  )
-select * from path
- -- group by path.src;
+ select * from beacon where beacon.dst = 1125302338 limit 1;
+
+-- perform a full graph traversal and returns a table with the
+-- cost of reaching each node until the destination. This can be
+-- used to properly route the user to its destination
+with recursive
+ graph_traversal(src, dst, cost) as (
+    values (null, 2708331052, 0)
+        union all
+    select link.src, link.dst, round(link.distance + graph_traversal.cost) as cost
+     from graph_traversal
+      join link on link.src = graph_traversal.dst
+      where round(link.distance + graph_traversal.cost) <= 760
+      order by cost
+ )
+ select * from graph_traversal;
