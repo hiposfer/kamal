45e59f53900fa425446162a1f4ce2a40b27c4fcb
Camilo Roca <camilo.roca@cobi.bike>
2019-06-15 22:33:49 +0200

    make a proper shortest path recursive query

--- resources/sqlite/queries.sql
+++ resources/sqlite/queries.sql
@@ -6,6 +6,7 @@ select * from link join node as source on link.src = source.id
                    join node as destination on link.dst = destination.id
 
 -- src 2708331052, dst 561065
+
 -- "sends a radar beacon to know the distance to the destination"
 -- This query is only useful to know the cost of the shortest path
 -- until the destination. We can use it to display it to the user
@@ -22,10 +23,12 @@ with recursive
  )
  select * from beacon where beacon.dst = 1125302338 limit 1;
 
--- perform a full graph traversal and returns a table with the
--- cost of reaching each node until the destination. This can be
--- used to properly route the user to its destination
+-- compute the shortest path from source to destination using
+-- a plain dijkstra algorithm; done here in several steps due
+-- to SQL restrictions
 with recursive
+ -- traverse the graph until the cost to target is reached. At that point we
+ -- would have a big traversal tree with repeated nodes but different weights
  graph_traversal(src, dst, cost) as (
     values (null, 2708331052, 0)
         union all
@@ -34,5 +37,24 @@ with recursive
       join link on link.src = graph_traversal.dst
       where round(link.distance + graph_traversal.cost) <= 760
       order by cost
+ ),
+ -- perform a proper dijkstra by keeping only the nodes from the tree with the
+ -- minimum cost
+ dijkstra as (
+    select graph_traversal.src, graph_traversal.dst, min(graph_traversal.cost) as cost
+      from graph_traversal
+      group by graph_traversal.dst
+      order by cost
+ ),
+ -- compute the shortest path by backtracking from the destination to the
+ -- source
+ shortest_path as (
+    select * from dijkstra where dijkstra.dst = 1125302338
+        union all
+    select dijkstra.*
+    from shortest_path, dijkstra
+    where dijkstra.dst = shortest_path.src
  )
- select * from graph_traversal;
+ -- the src of each row is redundant so we can remove it ;)
+ select shortest_path.dst, shortest_path.cost
+  from shortest_path;
